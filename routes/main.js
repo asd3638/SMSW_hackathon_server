70158fda18a4f66ec251dc126cd7281af67e55ed
daon <daon318@gmail.com>
2021-11-03 16:20:44 +0900

    merge

--- routes/main.js
+++ routes/main.js
@@ -1,20 +1,6 @@
 var express = require("express");
 var router = express.Router();
 
-/*
-const authenticateUser = (req, res, next) => {
-  if (req.isAuthenticated()) {
-    next();
-  } else {
-    res.status(301).redirect('auth/login');
-  }
-};
-
-router.get('/', authenticateUser, (req, res, next) => {
-  res.render('main', { user: req.user });
-});
-*/
-
 router.get("/", function (req, res) {
   res.render("main", { user: req.user });
 });
