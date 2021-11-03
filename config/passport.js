c3c059b6ce6c0a15fd63ccd67ab37af822856a57
ur2e <dbfl9711@gmail.com>
2021-11-03 21:03:11 +0900

    Modify Token update

--- config/passport.js
+++ config/passport.js
@@ -78,9 +78,9 @@ try {
   });
   if (exUser) { 
     const kakaoToken = await Token.update({
-      accessToken: accessToken,
-      where: {email: profile._json.kakao_account.email}
-    })
+      accessToken: accessToken},
+      {where: {email: profile._json.kakao_account.email}}
+    )
     return done(null, exUser, kakaoToken);
   } else {
     const hashedPassword = await bcrypt.hash(profile.displayName, 11);
