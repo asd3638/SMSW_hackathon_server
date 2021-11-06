cffe98d95508372064d6d3579b97839034be9544
daon <daon318@gmail.com>
2021-11-06 13:56:18 +0900

    return coupon

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(passport.session());
 
 app.use("/auth", require("./routes/auth"));
 app.use("/api", require("./routes/user"));
-app.use("/api/board", require("./routes/post"));
+app.use("/api/coupon", require("./routes/coupon"));
 
 // Port setting
 var port = 8080;
