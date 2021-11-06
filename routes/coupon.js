84d701d7ee97542abf9410554189826067122adf
ur2e <dbfl9711@gmail.com>
2021-11-07 03:44:43 +0900

    [Add] modify user info

--- routes/coupon.js
+++ routes/coupon.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { Coupon, Symbol } = require('../models');
 
 
-//사용자 쿠폰 정보 넘겨주기(MyPage용)
+// 사용자 쿠폰 정보 넘겨주기(MyPage용)
 router.get('/:id', async (req, res, next) => {
   try {
     const coupon = await Coupon.findAll(
@@ -20,27 +20,6 @@ router.get('/:id', async (req, res, next) => {
     next(error);
   }
 });
-
-/*
- * ㄷ ㅏ오나...
-
-     여기 수정해  . .  .. .  * 
- 
-//위드숙명 심볼 클릭 시 그 심볼해당 가게 출력
-router.get('/:id', async (req, res, next) => {
-  try {
-    const coupon = await Coupon.findAll({ where: { user_id: req.params.id } });
-    if (coupon) {
-      res.status(200).send(coupon);
-    } else {
-      res.status(400).send('no coupon');
-    }
-  } catch (error) {
-    console.error(error);
-    next(error);
-  }
-});
-*/
 
 //지도에서 가게 마커 눌렀을 때 가게의 심볼 + 갖고 있는 쿠폰 출력
 router.get('/:user_id/:store_id', async (req, res, next) => {
