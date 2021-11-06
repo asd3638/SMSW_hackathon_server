ede09819fd7a6f86d68fc63f12c9559f645cb276
ur2e <dbfl9711@gmail.com>
2021-11-06 17:12:45 +0900

    [ADD]get coupon count

--- routes/store.js
+++ routes/store.js
@@ -6,21 +6,53 @@ const Token = require("../models/token");
 const Store = require("../models/store");
 const Coupon = require("../models/coupon");
 
+// user-coupon 간 제약
+User.hasMany(Coupon, {
+    foreignKey: 'id'
+});
+Coupon.belongsTo(User, {
+    foreignKey: 'id'
+});
+
+// store-coupon간 제약
+Store.hasMany(Coupon, {
+    foreignKey: 'id'
+});
+Coupon.belongsTo(Store, {
+    foreignKey: 'id'
+});
+
 
 // get all store
 router.get("/", async (req, res) => {
     try {
-      const stores = await Store.findAll({raw: true});
-      if(stores) {
-        res.status(200).send(stores);
-      } else {
-        res.status(400).send("NO STORES");
-      }
-      
-    } catch (error) {
-      console.error(error);
-      res.send(error)
-    }
-  });
+        const stores = await Store.findAll({ raw: true });
+        console.log(stores);
+        if (stores) {
+            res.status(200).json(stores);
+        } else {
+            res.status(400).send("NO STORES");
+        }
 
-  module.exports = router;
+    } catch (error) {
+        console.error(error);
+        res.send(error);
+    }
+});
+
+module.exports = router;
+
+// get all store with user_id 
+router.get("/:id", async (req, res) => {
+    const user_id = req.params.id;
+    try {
+        const stores = await Store.findAll({ raw: true });
+        stores.forEach( async (value)=>{ 
+            const store_id = value.id;
+            const count = await Coupon.count({ where: { user_id: user_id, store_id: store_id } });
+            console.log(count);
+        });
+    } catch (error) {
+        console.error(error);
+    }
+});
---
+++ routes/test.js
@@ -0,0 +1,23 @@
+const stores = [
+    {
+        "id": 1,
+        "name": "배스킨라빈스",
+        "lat": 37.5445206377279,
+        "lng": 126.9680305023392,
+        "img": null
+    },
+    {
+        "id": 3,
+        "name": "달볶이",
+        "lat": 37.54512123656426,
+        "lng": 126.96565034335396,
+        "img": null
+    }
+]
+
+const user_id = 1;
+
+stores.forEach((value)=>{ 
+    const store_id = value.id;
+    console.log(value.id); 
+});
