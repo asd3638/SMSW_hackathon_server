3da01c9c82aa5de3b69d7aada3b9e0b9146cc99f
jihye <asd36384@gmail.com>
2021-11-07 17:00:56 +0900

    a

--- routes/symbol.js
+++ routes/symbol.js
@@ -1,38 +1,38 @@
 const { response } = require("express");
-const sequelize = require('sequelize');
+const sequelize = require("sequelize");
 var express = require("express");
 var router = express.Router();
-const { Symbol, Store } = require('../models');
+const { Symbol, Store } = require("../models");
 
 //모든 심볼 타입 출력
-router.get('/', async(req,res) => {
-    try {
-        const symbol = await Symbol.findAll({
-            attributes: [[sequelize.fn('DISTINCT', sequelize.col('symbol_type')), 'symbol_type']],
-        });
-        res.status(200).send(symbol);
-    } catch(err) {
-        console.log(err);
-    }
-    
-})
+router.get("/", async (req, res) => {
+  try {
+    const symbol = await Symbol.findAll({
+      attributes: [
+        [sequelize.fn("DISTINCT", sequelize.col("symbol_type")), "symbol_type"],
+      ],
+    });
+    res.status(200).send(symbol);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 // 심볼 별 가게 검색
-router.post('/', async(req, res) => {
-    try {
-        const type = req.body.symbol_type;
-        const stores = await Symbol.findAll({where: {symbol_type: type}});
-        const symbol_list = [];
-        for (var i = 0; i<stores.length; i++){
-            var st_id = stores[i].store_id;
-            const store = await Store.findOne({where: {id: st_id}});
-            symbol_list.push(store);
-        }
-        res.status(200).send(symbol_list);
-    } catch(err) {
-        console.log(err);
+router.post("/", async (req, res) => {
+  try {
+    const type = req.body.symbol_type;
+    const stores = await Symbol.findAll({ where: { symbol_type: type } });
+    const symbol_list = [];
+    for (var i = 0; i < stores.length; i++) {
+      var st_id = stores[i].store_id;
+      const store = await Store.findOne({ where: { id: st_id } });
+      symbol_list.push(store);
     }
-})
+    res.status(200).send(symbol_list);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 module.exports = router;
-
